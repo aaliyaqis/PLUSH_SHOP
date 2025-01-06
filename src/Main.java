5f8175712de7eb198295a394ced6eb1b9a45e857
aaliyaqis <2023263026@student.uitm.edu.my>
2025-01-06 16:25:17 +0800

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HIIIIIIIIIII!");
+        System.out.println("HIIIIIIIIIII HHHHHHHIIIIIIII!");
         System.out.println("Hello, World!");
     }
 }
