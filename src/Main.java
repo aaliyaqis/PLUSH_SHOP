23b31f1bf5729f4bfe29971c6c84a9eb5f2cf543
hvsnashrimp <2023239236@student.uitm.edu.my>
2025-01-09 10:12:18 +0800

    9/1/2025

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("HIIIIIIIIIII HHHHHHHIIIIIIII!");
         System.out.println("Hello, World!");
+        System.out.println("DUH");
     }
 }
